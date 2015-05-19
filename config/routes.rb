8e8f16609959febc79522dc6e58b0c05e8ce9225
Vitalie Danu <vitalie.dev@gmail.com>
2015-05-19 17:44:28 +0300

    config devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
